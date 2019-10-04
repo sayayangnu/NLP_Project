e64bb42ad2121c5a9b02d0499c29ea95664582c9
sayayangnu <ty254@georgetown.edu>
2019-10-04 18:26:03 -0400

    updated_main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Created on Fri Oct  4 15:57:04 2019
 
 import os
 
-os.chdir('/Users/chenlimin/Desktop/GU course/term3/nlp/project/x11')
+##os.chdir('/Users/chenlimin/Desktop/GU course/term3/nlp/project/x11')
 
 
 #from logistic_trial.py import *
@@ -18,12 +18,31 @@ from  NaiveBayes import *
 
 d1 = load_data('./P1_Data/Gold/merged.txt')
 
-d1.shape
+test_param = [{"hashtag":1, "emo":0, "smiley":0, "stem":1, "stop":1, "nut":1}, # delete emojis
+               {"hashtag":0, "emo":1, "smiley":1, "stem":1, "stop":1, "nut":1}, # keep hastag
+               {"hashtag":1, "emo":1, "smiley":1, "stem":1, "stop":1, "nut":0}, # no negation
+               {"hashtag":1, "emo":1, "smiley":1, "stem":1, "stop":1, "nut":1}]
+
+def get_best_param(test_params):
+    params = []
+    accuracy= []
+    for i in range(len((test_params))):
+        param = test_params[i]
+        df= smart_preprocessing2(df=d1[:500], **param)
+        ### NOW THIS IS ONLY d1[:500] AS SAMPLE CODE #####
+        accuracy.append(nb_cv(df))
+        params.append(param)
+    result = pd.DataFrame({'Params':params, "Accuracy":accuracy})
+    best_param = result['Params'][result['Accuracy'].idxmax()]
+    best_accuracy = result['Accuracy'].max()
+    return best_param,best_accuracy
 
 
-d1.head(20)
+    
 
-d2 = smart_preprocessing2(d1, hashtag=1, emo=1, smiley=1, stem=1, stop=1, bow=1, nut=1)
-
+    
+    
+    
+    
 
 
